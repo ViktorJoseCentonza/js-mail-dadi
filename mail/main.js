c6f0d72404ce67a817a82166dfcbc06b543d662b
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-13 16:13:07 +0100

    fix wrong logic

--- mail/main.js
+++ mail/main.js
@@ -12,9 +12,11 @@ const mail_list = ["giovanni@gmail.com", "francesco@gmail.com", "ludovico@gmail.
 const user_mail = prompt("inserisci la tua mail per la verifica a database.")
 let found = false;
 
+
 for (let i = 0; found == false; i++) { //keep going until email is found
     console.log(`check n*${i + 1}`);
-    if (typeof mail_list[i] != 'undefined') { //break if array cell is empty (end of array)
+    console.log(mail_list[i]);
+    if (typeof mail_list[i] == 'undefined') { //break if array cell is empty (end of array)
         alert(`access denied. ${user_mail} does not exist`)
         break;
     } else if (mail_list[i] == user_mail) { //break if email found
