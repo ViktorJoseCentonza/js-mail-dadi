0f56b4c202d17ca0b4f008aed85a056df2e3118e
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-13 14:49:22 +0100

    add mail+mail bonus

--- mail/bonus/main.js
+++ mail/bonus/main.js
@@ -10,17 +10,9 @@ Dopo aver svolto l'esercizio con il for  fai una cartella bonus e svolgilo anche
 
 const mail_list = ["giovanni@gmail.com", "francesco@gmail.com", "ludovico@gmail.com", "paolo@gmail.com"];
 const user_mail = prompt("inserisci la tua mail per la verifica a database.")
-let found = false;
 
-for (let i = 0; found == false; i++) { //keep going until email is found
-    console.log(`check n*${i + 1}`);
-    if (typeof mail_list[i] != 'undefined') { //break if array cell is empty (end of array)
-        alert(`access denied. ${user_mail} does not exist`)
-        break;
-    } else if (mail_list[i] == user_mail) { //break if email found
-        alert(`access granted! Welcome ${user_mail}`)
-        found = true;
-        break;
-    }
+if (mail_list.indexOf(user_mail) != -1) {
+    alert("access granted!")
+} else {
+    alert("access denied.");
 }
-
